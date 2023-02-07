7cc55c5d59294481a66a4e8bd4ee24ecaa263dd4
Brian Barnes Jr <houdinii1984@gmail.com>
2023-02-07 12:00:30 -0700

    add dark mode slider

--- scripts.js
+++ scripts.js
@@ -15,16 +15,48 @@ const metersToFeet = (meter) => meter * 3.28084;
 const literToGallon = (liter) => liter * 0.264172;
 const kilogramToPound = (kg) => kg * 2.20462;
 
+// region Switches
+
+// Simple css to style it like a toggle switch
+// Tutorial:
+// https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
+
+const modeToggleSwitch = document.querySelector('#mode-checkbox');
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    modeToggleSwitch.checked = true;
+  }
+}
+
+modeToggleSwitch.addEventListener('change', switchTheme, false);
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark'); //add this
+  }
+  else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light'); //add this
+  }
+}
+// endregion
+
 function renderConversions(){
   let inputVal = inputEl.value;
 
   let imperialLengthResult = feetToMeters(inputVal).toFixed(3);
-  let imperialVolumeResult = gallonToLiter(inputVal).toFixed(4);
-  let imperialMassResult = poundToKilogram(inputVal).toFixed(5);
+  let imperialVolumeResult = gallonToLiter(inputVal).toFixed(3);
+  let imperialMassResult = poundToKilogram(inputVal).toFixed(3);
 
   let metricLengthResult = metersToFeet(inputVal).toFixed(3);
-  let metricVolumeResult = literToGallon(inputVal).toFixed(4);
-  let metricMassResult = kilogramToPound(inputVal).toFixed(5);
+  let metricVolumeResult = literToGallon(inputVal).toFixed(3);
+  let metricMassResult = kilogramToPound(inputVal).toFixed(3);
 
   lengthResultEl.textContent = `${inputVal} feet = ${imperialLengthResult} meters | ${inputVal} meters = ${metricLengthResult} feet`
   volumeResultEl.textContent = `${inputVal} gallons = ${imperialVolumeResult} liters | ${inputVal} liters = ${metricVolumeResult} gallons`
